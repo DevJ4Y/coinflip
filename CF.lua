f94cfef7db13a7459eb40cc12e3c9129048bb8db
ZoPotato <JCBiddlecombe@gmail.com>
2019-04-29 09:20:39 +0100

    Basic HUD added

--- CF.lua
+++ CF.lua
@@ -5,13 +5,15 @@ local multiYote = 1.5
 local ohFuckingYeet = 0
 local currentTeam
 local op;
+local stage
+local yeetornoyeet
 local function newFlip()
     currentTeam = math.random(2)
-    print("New flip -- Team: " .. currentTeam)
     net.Start( "CFCreateGame" )
         net.WriteUInt(betAmount, 32 )
         net.WriteUInt(currentTeam, 2 )
     net.SendToServer()
+    stage = 1
 end
 net.Receive("CFNewGame", function()
     op = net.ReadEntity()
@@ -19,20 +21,44 @@ net.Receive("CFNewGame", function()
     PP = net.ReadUInt(2)
     HURTS = net.ReadUInt(32)
     winner = net.ReadUInt(2)
-    print("Flip started with " .. op:GetName())
-    print("Winning team: " .. winner)
+    stage = 2
     if winner == currentTeam then
-        print("Flip Won")
+        yeetornoyeet = true
         ohFuckingYeet = ohFuckingYeet + MY
         betAmount = MY * multiYote
         multiYote = multiYote + 0.1
     else
-        print("Flip Lost")
+        yeetornoyeet = false
         ohFuckingYeet = ohFuckingYeet - MY
         betAmount = CF.MinFlipPrice * 10
         multiYote = 1.5
     end
-    print("Total profit: " .. ohFuckingYeet)
     timer.Simple( math.random(yeetish + 3,yeetish + 10), newFlip)
 end)
 newFlip()
+
+
+hook.Add( "HUDPaint", "Whydontyoufuckoffalready", function()
+	draw.RoundedBoxEx(50,0,100,300,135,Color(0,144,255,125),false,true,false,true)
+    surface.SetFont("Trebuchet24")
+    surface.SetTextColor(Color(255,255,255))
+    if stage == 1 then
+        surface.SetTextPos(5, 105)
+        surface.DrawText("Flip pending")
+    elseif stage == 2 then
+        surface.SetTextPos(5, 105)
+        surface.DrawText("Flip started with:")
+        surface.SetTextPos(5, 130)
+        surface.DrawText(op:GetName())
+        surface.SetTextPos(5, 155)
+        if yeetornoyeet then
+            surface.DrawText("Flip Won")
+        else
+            surface.DrawText("Flip Lost")
+        end
+    end
+    surface.SetTextPos(5, 180)
+    surface.DrawText("Bet amount: " .. betAmount)
+    surface.SetTextPos(5, 205)
+    surface.DrawText("Total profit: " .. ohFuckingYeet)
+end)
